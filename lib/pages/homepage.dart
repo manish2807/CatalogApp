28d7bf166d562021ab914456365d040569c129f4
Manish Kumar <manishkumar@Manishs-MacBook-Pro.local>
2021-09-28 22:15:59 +0530

    List view changed to Grid

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -5,60 +5,6 @@ import 'package:catalog_app/widgets/item_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
-// class Homepage extends StatefulWidget {
-//   @override
-//   State<Homepage> createState() => _HomepageState();
-// }
-
-// class _HomepageState extends State<Homepage> {
-//   final int days = 30;
-
-//   final String name = "Flutter";
-//   @override
-//   void initState() {
-//     super.initState();
-//     loadData();
-//   }
-
-//   loadData() async {
-//     await Future.delayed(Duration(seconds: 2));
-//     final catalogJson =
-//         await rootBundle.loadString("assets/files/catalog.json");
-//     final decodedData = jsonDecode(catalogJson);
-//     var productsData = decodedData["products"];
-//     CatalogModel.items = List.from(productsData)
-//         .map<Item>((item) => Item.fromMap(item))
-//         .toList();
-//     setState(() {});
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text(("Catalog App")),
-//         centerTitle: true,
-//       ),
-//       body: Padding(
-//         padding: const EdgeInsets.all(16.0),
-//         child: (CatalogModel.items != null && CatalogModel.items.isNotEmpty)
-//             ? ListView.builder(
-//                 itemCount: CatalogModel.items.length,
-//                 itemBuilder: (context, index) {
-//                   return ItemWidget(
-//                     item: CatalogModel.items[index],
-//                   );
-//                 },
-//               )
-//             : Center(
-//                 child: CircularProgressIndicator(),
-//               ),
-//       ),
-//       drawer: MyDrawer(),
-//     );
-//   }
-// }
-
 class Homepage extends StatefulWidget {
   @override
   _HomepageState createState() => _HomepageState();
@@ -96,11 +42,40 @@ class _HomepageState extends State<Homepage> {
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: (CatalogModel.items.isNotEmpty)
-            ? ListView.builder(
+            ? GridView.builder(
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    mainAxisSpacing: 11,
+                    crossAxisSpacing: 11),
+                itemBuilder: (context, index) {
+                  final item = CatalogModel.items[index];
+                  return Card(
+                      clipBehavior: Clip.antiAlias,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10)),
+                      child: GridTile(
+                        header: Container(
+                            child: Text(
+                              item.name,
+                              style: TextStyle(color: Colors.white),
+                            ),
+                            padding: const EdgeInsets.all(12),
+                            decoration: BoxDecoration(
+                              color: Colors.red,
+                            )),
+                        child: Image.network(item.image),
+                        footer: Container(
+                            child: Text(
+                              item.price.toString(),
+                              style: TextStyle(color: Colors.white),
+                            ),
+                            padding: const EdgeInsets.all(12),
+                            decoration: BoxDecoration(
+                              color: Colors.black38,
+                            )),
+                      ));
+                },
                 itemCount: CatalogModel.items.length,
-                itemBuilder: (context, index) => ItemWidget(
-                  item: CatalogModel.items[index],
-                ),
               )
             : Center(
                 child: CircularProgressIndicator(),
